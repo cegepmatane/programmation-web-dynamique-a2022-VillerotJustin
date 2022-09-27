d20cbcf5644658dd3cfe9dfe7b615cb5649df62e
VILLEROT Justin <justinvillerot@orange.fr>
2022-09-27 17:08:08 -0400

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$tittle = "Home";
+require 'header.php';
+?>
+<div id="page-container">
+    <div id="content-wrap">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tribe-list.php">Tribes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="membre.php">Membre</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="recherche-avancee.php">Recherche</a>
+            </li>
+        </ul>
+    </div>
+    <a href="lost.php" style="color: rgba(0, 0, 0, 0);">Fblthp</a>
+<?php
+
+require 'footer.php';
+?>
---
+++ lost.php
@@ -0,0 +1,13 @@
+<?php
+
+$tittle = "Lost";
+require 'header.php';
+?>
+<div id="page-container">
+    <div id="content-wrap" class="longlivecenter">
+        <h1>The website seems to have a problem</h1>
+        <img src="images/Fblthpthelost_1200x.webp">
+    </div>
+    <?php
+    require 'footer.php';
+    ?>
